102ce145e99805824eefa57a8f3b4a8b7d90e84a
Logan Thompson <logant314159@gmail.com>
2023-04-29 08:11:53 -0500

    Implemented simple PPM renderer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::*;
+use std::io::*;
+
+const IMAGE_HEIGHT:i32  = 256;
+const IMAGE_WIDTH: i32 = 256;
+
+fn generate_image() -> String {
+    let mut image = String::new();
+    image.push_str(&format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT));
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let b = 0.25;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            image.push_str(&format!("{} {} {}\n", ir, ig, ib))
+        }
+    }
+
+    image
+}
+
 fn main() {
-    println!("Hello, world!");
+    let image = generate_image();
+    let mut file = File::create("image2.ppm").unwrap();
+    file.write_all(image.as_bytes()).unwrap();
 }
