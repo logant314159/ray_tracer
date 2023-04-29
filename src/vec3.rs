b8cc6ebd73d27a18fd3d70806b3734dc64aa4f93
Logan Thompson <logant314159@gmail.com>
2023-04-29 10:56:38 -0500

    Implemented Index[] operator for Vec3 struct.

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,15 +1,15 @@
-use std::ops::{Add, Sub, Mul, Div, Neg};
+use std::ops::{Add, Sub, Index, Mul, Div, Neg};
 
 pub struct Vec3 {
-    e: [f64; 3],
+    e: [f32; 3],
 }
 
 impl Vec3 {
     pub fn new(e0: f32, e1: f32, e2: f32) -> Vec3 { Vec3 { e: [e0, e1, e2] } }
 
-    pub fn x(&self) -> f64 { self.e[0] }
-    pub fn y(&self) -> f64 { self.e[1] }
-    pub fn z(&self) -> f64 { self.e[2] }
+    pub fn x(&self) -> f32 { self.e[0] }
+    pub fn y(&self) -> f32 { self.e[1] }
+    pub fn z(&self) -> f32 { self.e[2] }
 }
 
 impl Add for Vec3 {
@@ -32,4 +32,9 @@ impl Sub for Vec3 {
                 self.e[2] - other.e[2]]
         }
     }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+    fn index(&self, index: usize) -> &f32 { &self.e[index] }
 }
