eef6065cb462f56ac67cc81f3946f4c355104e4f
Logan Thompson <logant314159@gmail.com>
2023-04-29 08:38:37 -0500

    Added scanline progress tracker.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::fs::*;
 use std::io::*;
 
-const IMAGE_HEIGHT:i32  = 256;
-const IMAGE_WIDTH: i32 = 256;
+const IMAGE_HEIGHT:i32  = 1080;
+const IMAGE_WIDTH: i32 = 1920;
 
 fn generate_image() -> String {
     let mut image = String::new();
     image.push_str(&format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT));
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        print!("{}[2J", 27 as char); // Clear screen. (ANSI escape code.)
+        print!("\rScanlines remaining: {} ", j);
+
         for i in 0..IMAGE_WIDTH {
             let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
             let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
