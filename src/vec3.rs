265d4dab78d033e30cb9b9c5a5bb45f30f614c77
Logan Thompson <logant314159@gmail.com>
2023-04-29 10:20:18 -0500

    Implemented + operator for Vec3 struct.

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub, Mul, Div, Neg};
+
 pub struct Vec3 {
     e: [f64; 3],
 }
@@ -5,5 +7,16 @@ pub struct Vec3 {
 impl Vec3 {
     pub fn new(e0: f32, e1: f32, e2: f32) -> Vec3 {
         Vec3 { e: [e0, e1, e2] }
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            e: [self.e[0] + other.e[0],
+                self.e[1] + other.e[1],
+                self.e[2] + other.e[2]]
+        }
     }
 }
