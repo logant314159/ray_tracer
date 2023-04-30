3fee2afe31a796cc9651370e374dc056d7ee4c75
Logan Thompson <logant314159@gmail.com>
2023-04-30 14:31:51 -0500

    Implemented sky ray tracing functionality.

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,53 @@ use std::fs::*;
 use std::io::*;
 
 mod vec3; // Magically link vec3.rs to this module by name at compile time.
+mod ray;
 use vec3::Vec3;
+use ray::Ray;
 
-const IMAGE_HEIGHT:i32  = 256;
+// Image dimensions.
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
 const IMAGE_WIDTH: i32 = 256;
+const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as i32;
+
+// Camera details.
+const VIEWPORT_HEIGHT: f32 = 2.0;
+const VIEWPORT_WIDTH: f32 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+const FOCAL_LENGTH: f32 = 1.0;
+const CAMERA_ORIGIN: Vec3 = Vec3 { e: [0.0, 0.0, 0.0] };
 
 /// Converts the 0 -> 1 f32 values of a Vec3 into a usable PPM pixel string.
 fn make_pixel(pixel_color: Vec3) -> String {
     format!("{} {} {}\n", 255.99 * pixel_color[0], 255.99 * pixel_color[1], 255.99 * pixel_color[2])
 }
 
-fn generate_image() -> String {
-    let mut image = String::new();
-    image.push_str(&format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT));
+/// For a given ray 
+fn ray_color(r: &Ray) -> Vec3 {
+    let unit_direction = r.direction.unit_vector();
+    let t = 0.5 * (unit_direction[1] + 1.0);
+    Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
 
+/// Writes a PPM file to current directory.
+fn render(vert: Vec3, horz: Vec3, llc: Vec3) {
+    let mut file = File::create("image.ppm").unwrap();
+    file.write_all(format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT).as_bytes()).unwrap();
     for j in (0..IMAGE_HEIGHT).rev() {
-        print!("{}[2J", 27 as char); // Clear screen. (ANSI escape code.)
-        print!("\rScanlines remaining: {} ", j);
-
         for i in 0..IMAGE_WIDTH {
-            let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
-            let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let b = 0.25;
-
-            image.push_str(&make_pixel(Vec3::new(r, g, b)))
+            let u = (i as f32) / ((IMAGE_WIDTH - 1) as f32);
+            let v = (j as f32) / ((IMAGE_HEIGHT - 1) as f32);
+            let r = Ray::new(CAMERA_ORIGIN, llc + horz * u + vert * v - CAMERA_ORIGIN);
+            let pixel_color = ray_color(&r);
+            file.write_all(make_pixel(pixel_color).as_bytes()).unwrap();
         }
     }
-
-    image
 }
 
 fn main() {
-    let image = generate_image();
-    let mut file = File::create("image3.ppm").unwrap();
-    file.write_all(image.as_bytes()).unwrap();
+    // I would love to make these constants but rust doesn't allow for floating-point operations at compile time.
+    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let lower_left_corner = CAMERA_ORIGIN - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+
+    render(vertical, horizontal, lower_left_corner);
 }
