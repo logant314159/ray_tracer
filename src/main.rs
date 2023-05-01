f73b33a355bdb92e6982500df09421cf0e49ada6
Logan Thompson <logant314159@gmail.com>
2023-05-01 10:56:15 -0500

    Simplified hit_sphere().

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,12 @@ fn make_pixel(pixel_color: Vec3) -> String {
 
 fn hit_sphere(center: Vec3, radius: f32, r: &Ray) -> f32 {
     let oc = r.origin - center;
-    let a = r.direction.dot(r.direction);
-    let b = 2.0 * oc.dot(r.direction);
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant < 0.0 { return -1.0; }
-    else { return (-b - discriminant.sqrt()) / (2.0 * a);  }
+    let a = r.direction.length_squared();
+    let half_b = oc.dot(r.direction);
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+    if discriminant < 0.0 { -1.0 }
+    else { (-half_b - discriminant.sqrt()) / a }
 }
 
 /// For a given ray 
