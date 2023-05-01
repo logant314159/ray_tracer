b1c9ae7d47dcb9ad9b2a54b813e1205570f47678
Logan Thompson <logant314159@gmail.com>
2023-05-01 11:44:07 -0500

    Made hittable.rs more rusty.

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,15 +1,14 @@
 use crate::Vec3;
+use crate::Ray;
 
-pub struct Hit_Record {
-    pub p: Vec3,
-    pub normal: Vec3,
-    pub t: f32
+pub struct HitRecord {
+    t: f32,
+    p: Vec3,
+    normal: Vec3,
 }
 
-pub struct Hittable;
-
-impl Hittable {
-    pub fn hit(&self, r: Ray, t_min: f32, t_max: f32, rec: &mut Hit_Record) -> bool {
+trait Hittable {
+    fn hit(r: &Ray, t_min: f32, t_max: f32, rec: &HitRecord) -> bool {
         false
     }
 }
