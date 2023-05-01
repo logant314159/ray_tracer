4234a0cfeb4deff33c4c2b15b320ad1bbd51af6c
Logan Thompson <logant314159@gmail.com>
2023-05-01 14:38:45 -0500

    Added camera struct and implemented anti=aliasing.

---
+++ src/camera.rs
@@ -0,0 +1,24 @@
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+
+pub struct Camera {
+    origin: Vec3,
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Camera {
+    pub fn camera() -> Self {
+        Camera { // These values are hardcoded, but it wouldn't be hard to make them configurable
+            lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
+            horizontal: Vec3::new(4.0, 0.0, 0.0),
+            vertical: Vec3::new(0.0, 2.0, 0.0),
+            origin: Vec3::new(0.0, 0.0, 0.0),
+        }
+    }
+
+    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+        Ray::ray(self.origin, self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ mod hittable_list;
 mod ray;
 mod sphere;
 mod vec3;
+mod camera;
 
 use std::fs::*;
 use std::io::*;
+use rand::prelude::*;
 
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
 use ray::Ray;
 use sphere::Sphere;
 use vec3::Vec3;
+use camera::Camera;
 
 fn color(r: &Ray, world: &HittableList) -> Vec3 {
     let mut rec = HitRecord::default();
@@ -31,30 +34,34 @@ fn color(r: &Ray, world: &HittableList) -> Vec3 {
 }
 
 fn render(world: HittableList) {
-    let w = 2000;
-    let h = 1000;
+    let w = 400;
+    let h = 200;
+    let samples = 100;
     let max_value = 255;
-
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
 
     let mut image_file = File::create("image.ppm").unwrap();
     image_file.write_all(format!("P3\n{} {}\n{}\n", w, h, max_value).as_bytes()).unwrap();
 
-    let mut stdout = stdout();
+    let cam = Camera::camera();
+    let mut rng = rand::thread_rng();
 
+    let mut stdout = stdout();
     for j in (0..h).rev() {
         write!(stdout, "\rScanlines remaining: {}", j).unwrap();
         stdout.flush().unwrap();
         for i in 0..w {
-            let u = i as f32 / w as f32;
-            let v = j as f32 / h as f32;
+            let mut col = Vec3::default();
 
-            let r = Ray::ray(origin, lower_left_corner + horizontal * u + vertical * v);
-            let p = r.point_at_parameter(2.0);
-            let col = color(&r, &world);
+            for _ in 0..samples {
+                let u = (i as f32 + rng.gen::<f32>()) / w as f32;
+                let v = (j as f32 + rng.gen::<f32>()) / h as f32;
+    
+                let r = &cam.get_ray(u, v); //Ray::ray(origin, lower_left_corner + horizontal * u + vertical * v);
+                let p = r.point_at_parameter(2.0);
+                col = col + color(&r, &world);
+            }
+
+            col = col / samples as f32;
 
             let ir = (255.99 * col.r()) as i32;
             let ig = (255.99 * col.g()) as i32;
